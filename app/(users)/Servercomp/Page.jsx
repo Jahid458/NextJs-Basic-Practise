bfc0f4321640ac3734d1b28fc70b320be6b5989d
Jahid458 <Jahidul898@outlook.com>
2025-12-11 01:32:04 +0600

    server component add in post

--- app/(users)/Servercomp/Page.jsx
+++ app/(users)/Servercomp/Page.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+// app/servercomp/page.jsx
 
-export default function Page(){
+export default async function ServerComp() {
+  const URL = "https://jsonplaceholder.typicode.com/posts";
+
+  const res = await fetch(URL);
+  const data = await res.json();
+  console.log(data);
+
   return (
-    <div>Page</div>
-  )
+    <div>
+      <h1 className="text-2xl font-bold mb-5">Server Comp</h1>
+
+      <ul className="grid grid-cols-3 gap-5">
+        {data.map((curElem, index) => (
+          <li
+            key={index}
+            className="p-4 bg-gray-200 rounded-lg shadow-md"
+          >
+            {curElem.body}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
