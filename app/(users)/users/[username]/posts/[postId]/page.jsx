3cfb5f960b125f0698c59555e21466295e122736
Jahid458 <Jahidul898@outlook.com>
2025-12-17 23:10:34 +0600

    nested dynamic route added

---
+++ app/(users)/users/[username]/posts/[postId]/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SingleProfilePost = async (props) => {
+  console.log(props);
+
+  const user = await props.params;
+  console.log(user);
+  return <h1>user: {user.username}, postId: {user.postId}</h1>;
+};
+
+export default SingleProfilePost;
