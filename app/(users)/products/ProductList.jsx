eb370219794355dadd98598d36b4699a1b2db66f
Jahid458 <Jahidul898@outlook.com>
2025-12-19 00:37:23 +0600

    Object form entries

---
+++ app/(users)/products/ProductList.jsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { useSearchParams } from 'next/navigation'
+import React from 'react'
+
+const ProductList = () => {
+
+   const searchParams =  useSearchParams() ;
+   
+   const pages = searchParams.get('page');
+   const category = searchParams.get('category');
+   const entries = Object.fromEntries(searchParams.entries())
+
+
+
+   console.log("pages: " , pages);
+   console.log("Category: " , category);
+   console.log("Entries: " , entries);
+
+
+
+   return (
+    <div>
+        <h1>Client - {category}</h1> 
+    </div>
+  )
+}
+
+export default ProductList
--- app/(users)/products/page.jsx
+++ app/(users)/products/page.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import ProductList from "./ProductList";
 
 const Products = async ({ searchParams }) => {
   const Params = await searchParams;
-  // console.log(searchParams);
+  console.log("Outer: ",Params); 
 
   const category = Params?.category || "all";
   const sort = Params?.sort || "default";
   const page = Params?.page || 1;
   return (
     <div>
+        <ProductList />
       Showing {category} products,sorted by {sort}, page {page}
     </div>
   );
@@ -16,4 +18,4 @@ const Products = async ({ searchParams }) => {
 
 export default Products;
 
-//http://localhost:3000/products?category=laptop&page=8&sort=price_asc
+//  http://localhost:3000/products?category=laptop&page=8&sort=price_asc
